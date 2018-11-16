8b6a040001397ec23d78123b55a8d0e422e9ae9b
Antoine Balayssac <abalayss@insat.insa-toulouse.fr>
2018-11-16 15:18:12 +0100

    accelerometre fonctionne

--- VersionEtudiante/src/main.c
+++ VersionEtudiante/src/main.c
@@ -1,6 +1,6 @@
 #include "stm32f10x.h"
-
-
+#include "math.h"
+int x,y,tangue;
 /*fonction executée quand l'exception est levée
  (handler) */
  
@@ -17,10 +17,6 @@ void girouette()
 {
 	//on active les signaux d'horloge pour le GPIO A
 	RCC->APB1ENR|=RCC_APB1ENR_TIM3EN;
-	
-
-	
-	
 	//configuration broche 6 gpio A en input
 	GPIOA->CRL &= ~( (GPIO_CRL_MODE6_1) | (GPIO_CRL_MODE6_0));
 	//config de la broche 6 en floating input
@@ -93,17 +89,61 @@ void girouette()
 	//on autorise l'interruption au niveau du coeur
 	NVIC->ISER[0]|=NVIC_ISER_SETENA_23;
 }
+/* pour améliorer la précision, on peut faire une fenêtre glissante
+gardant en mémoire les n dernières mesures. En effet, un acclééromètre peut renvoyer
+une grosse mesure pour une petite pente s'il y a eu un gros accoût .
+Lancer le calcul toutes les x ms : pour cela régler une int sur deb d'un timer 
+avec un petit arr */
+void  accelerometre () {
+//on active les signaux d'horloge pour le GPIO A
+	RCC->APB1ENR|=RCC_APB1ENR_TIM3EN;
+	//configuration broche 0 gpio C en input
+	GPIOC->CRL &= ~( (GPIO_CRL_MODE0_1) | (GPIO_CRL_MODE0_0));
+	//config de la broche 0 en analog input
+	GPIOC->CRL&=~(GPIO_CRL_CNF0_0 | GPIO_CRL_CNF0_1);
+	
+	//configuration broche 1 gpio C en input
+	GPIOC->CRL &= ~( (GPIO_CRL_MODE1_1) | (GPIO_CRL_MODE1_0));
+	//config de la broche 1 en analog input
+	GPIOC->CRL &=~(GPIO_CRL_CNF1_0 | GPIO_CRL_CNF1_1);
+	
+	}
+void configure_adc(){
+
+    RCC->APB2ENR |= RCC_APB2ENR_ADC1EN; // validation horloge ADC1
+    RCC->CFGR |= RCC_CFGR_ADCPRE_DIV6; // passage de l'horloge ADC1 à 12MHz
+    ADC1->CR2|= ADC_CR2_ADON; // démarrage ADC1
+		ADC1->CR2|=0x07<<17;
+	  ADC1->CR2|=ADC_CR2_EXTTRIG;
+    ADC1->SQR1&= ADC_SQR1_L; // fixe le nombre de conversion à 1
+    ADC1->CR2 |= ADC_CR2_CAL; // dÈbut de la calibration
+    while ((ADC1->CR2 & ADC_CR2_CAL)); // attente de la fin de la calibration
 
 
+	
+	
+
+}
+int convert_single(char channel){
+	
+		ADC1->SQR3= channel; // indique la voie à convertir
+    ADC1->CR2 |= ADC_CR2_SWSTART; // lancement de la conversion
+    while(!(ADC1->SR & ADC_SR_EOC) ) {} // attente de la fin de conversion
+    ADC1->SR &= ~ADC_SR_EOC; // validation de la conversion
+    return ADC1->DR & ~((0x0F) << 12); // retour de la conversion
+}
 
 int main (void)
 {	
-	girouette();
-	
-	
-	while(1)
-    {  
-	
-    }
 
+	girouette();
+	accelerometre();
+	configure_adc();
+	
+	 while(1)
+	{	
+		x = convert_single(10); // conversion
+    y = convert_single(11); // conversion   
+		tangue = sin(x)/cos(y);
+	}
 }
